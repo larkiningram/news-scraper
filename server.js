8fa22a7761878e985118465191b7067d973fdafc
Larkin <larkiningram@gmail.com>
2019-10-27 15:25:55 -0500

    transferred code from unsolved scraping activity and started working on routes

--- server.js
+++ server.js
@@ -1,80 +1,135 @@
-// Dependencies
 var express = require("express");
-var mongojs = require("mongojs");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+
+// Our scraping tools
+// Axios is a promised-based http library, similar to jQuery's Ajax method
+// It works on the client and on the server
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+// Require all models
+var db = require("./models");
+
+var PORT = 3000;
 
 // Initialize Express
 var app = express();
 
-// Set up a static folder (public) for our web app
+// Configure middleware
+const Article = require("./models/article");
+const Comment = require("./models/comment");
+
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder
 app.use(express.static("public"));
 
-// Database configuration
-// Save the URL of our database as well as the name of our collection
-var databaseUrl = "scraper";
-var collections = ["scrapedData"];
-
-// Use mongojs to hook the database to the db variable
-var db = mongojs(databaseUrl, collections);
-
-// This makes sure that any errors are logged if mongodb runs into an issue
-db.on("error", function(error) {
-  console.log("Database Error:", error);
-});
+// Connect to the Mongo DB
+mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
 
 // Routes
-// 1. At the root path, send a simple hello world message to the browser
+
 app.get("/", function(req, res) {
-  res.send("Hello world");
+  res.send(index.html);
 });
+// A GET route for scraping the echoJS website
+app.get("/scrape", function (req, res) {
+  // First, we grab the body of the html with axios
+  axios.get("http://www.echojs.com/").then(function (response) {
+    // Then, we load that into cheerio and save it to $ for a shorthand selector
+    var $ = cheerio.load(response.data);
 
-// 2. At the "/all" path, display every entry in the animals collection
-app.get("/all", function(req, res) {
-  // Query: In our database, go to the animals collection, then "find" everything
-  db.scraper.find({}, function(error, found) {
-    // Log any errors if the server encounters one
-    if (error) {
-      console.log(error);
-    }
-    // Otherwise, send the result of this query to the browser
-    else {
-      res.json(found);
-    }
+    // Now, we grab every h2 within an article tag, and do the following:
+    $("article h2").each(function (i, element) {
+      // Save an empty result object
+      var result = {};
+
+      // Add the text and href of every link, and csave them as properties of the result object
+      result.title = $(this)
+        .children("a")
+        .text();
+      result.link = $(this)
+        .children("a")
+        .attr("href");
+
+      // Create a new Article using the `result` object built from scraping
+      db.Article.create(result)
+        .then(function (dbArticle) {
+          // View the added result in the console
+          console.log(dbArticle);
+        })
+        .catch(function (err) {
+          // If an error occurred, log it
+          console.log(err);
+        });
+    });
+
+    // Send a message to the client
+    res.send("Scrape Complete");
   });
 });
 
-// 4. At the "/weight" path, display every entry in the animals collection, sorted by weight
-app.get("/??", function(req, res) {
-    // Query: In our database, go to the animals collection, then "find" everything,
-    // but this time, sort it by weight (-1 means descending order)
-    db.scraper.find(), function(error, found) {
-      // Log any errors if the server encounters one
-      if (error) {
-        console.log(error);
-      }
-      // Otherwise, send the result of this query to the browser
-      else {
-        res.json(found);
-      }
+// Route for getting all Articles from the db
+app.get("/articles", function (req, res) {
+
+  // TODO: Finish the route so it grabs all of the articles
+  db.Article.find({}).then(function (dbArticle) {
+    console.log(dbArticle)
+    res.json(dbArticle);
+  });
+});
+
+// Route for grabbing a specific Article by id, populate it with it's comments
+app.get("/articles/:id", function (req, res) {
+  // TODO
+  // ====
+  // Finish the route so it finds one article using the req.params.id,
+  // and run the populate method with "comments",
+  // then responds with the article with the comments included
+
+  // db.Article.find({}).then(function(result) {
+  //   res.json(result);
+  // })
+
+  db.Article.find({ id: req.params.id }).populate("comment").then(function (dbArticle) {
+    res.json(dbArticle)
+  });
+
+});
+
+// Route for saving/updating an Article's associated comments
+app.post("/articles/:id", function (req, res) {
+  // TODO
+  // ====
+  // save the new comments that gets posted to the commentss collection
+  // then find an article from the req.params.id
+  // and update it's "comments" property with the _id of the new comments
+  var comment = new Comment({
+    title: req.body.title,
+    body: req.body.body
+  });
+  comment.save().then(function(err, result) {
+    if (err) {
+      throw err
+    } else {
+      console.log(result);
     }
   });
 
-// 4. At the "/weight" path, display every entry in the animals collection, sorted by weight
-app.get("/??", function(req, res) {
-  // Query: In our database, go to the animals collection, then "find" everything,
-  // but this time, sort it by weight (-1 means descending order)
-  db.scraper.find(), function(error, found) {
-    // Log any errors if the server encounters one
-    if (error) {
-      console.log(error);
-    }
-    // Otherwise, send the result of this query to the browser
-    else {
-      res.json(found);
-    }
-  }
+  // db.Comment.create(comment).then(function(dbComment) {
+  //   res.json(dbComment);
+  // });
+
+  db.Article.update({ id: req.params.id }, {$set: {_id: comment._id}}).then(function (dbArticle) {
+    res.json(dbArticle)
+  });
 });
 
-// Set the app to listen on port 3000
-app.listen(3000, function() {
-  console.log("App running on port 3000!");
+// Start the server
+app.listen(PORT, function () {
+  console.log("App running on port " + PORT + "!");
 });
